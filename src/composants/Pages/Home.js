49c28c6d27942e80a8e69bdd0bd50ef017cbf72d
Anaïs Zafiriou-Lejeune <anais.ljn@outlook.fr>
2024-02-28 20:46:42 +0100

    ajout de photos et agrandissement de la banierre

--- src/composants/Pages/Home.js
+++ src/composants/Pages/Home.js
@@ -9,10 +9,11 @@ function Home() {
                 <div className='presentation'>
                     <h2>Bienvenue à tous les Dogs Lovers</h2>
                     <div className='presentation-paragraph'>
-                        <p>Ce site est crée dans le cadre de ma formation de développeuse Front-end via Open Classroom. Il est développé avec le framework React basé sur les langages HTML, CSS et Javascript.</p>
-                        <p>J'ai mis en avant Pika cette pôtite chienne qui m'accompagne depuis bientôt 9 ans. Vous trouverez ici quelques photos
+                        <p>Ce site est crée dans le cadre de ma formation de développeuse Front-end avec le centre de formation Open Classroom. Il est développé avec le framework React basé sur les langages HTML, CSS et Javascript.</p>
+                        <p>J'ai mis en avant Pika, cette petite chienne Cairn Terrier/Ratier qui m'accompagne depuis 8 ans. Vous trouverez ici quelques photos
                             de chaque année de sa vie depuis son adoption avec également des anecdotes et des conseils lorsque l'on adopte un chien.
                         </p>
+                        <p></p>
                     </div>
                 </div>
                 {years.map((information) => <Thumb years={information} />)}
